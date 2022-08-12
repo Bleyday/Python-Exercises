87eb9e0116ee5af9bef8f26861c2232036717583
peirui <peirui.z@gmx.de>
2022-08-12 11:32:22 +0200

    letterspacing and reveal found letters + canvas for

--- Hangman/main.py
+++ Hangman/main.py
@@ -20,21 +20,46 @@ root.geometry("700x800")
 hangman = tk.Frame(root)
 hangman.pack()
 
-used = tk.Frame(root, bg="white")
+used = tk.Frame(root, bg="white", highlightbackground="black", highlightthickness=1, height=800)
 used.pack(side="right")
+
+word_frame = tk.Frame(root, highlightbackground="black", highlightthickness=1)
+word_frame.pack()
+
+strich_frame = tk.Frame(word_frame, highlightbackground="red", highlightthickness=1)
+strich_frame.pack(side="bottom")
 
 #Labels und Entrys
 used_label = tk.Label(used, text="benutzte Buchstaben")
 used_label.pack(side="top")
 
-hangman_label = tk.Label(hangman, relief="sunken", width=50, height=50)
-hangman_label.pack()
+#Hangman Canvas
+hangman_label = tk.Canvas(hangman, bd=2, relief="sunken",width=400, height=300)
+hangman_label.pack(pady=50)
+
+hangman_label.create_line(0, 0, 50, 20, fill="#476042", width=3)
 
 word_label = tk.Label(root, text=word)
 word_label.pack(side="top")
 
-letter_entry = tk.Entry(root)
-letter_entry.pack(side="bottom")
+letters = list(word)
+
+for i in range(len(letters)-1):
+    globals()['letter%s' % i] = tk.Label(word_frame, text="h")
+    globals()['letter%s' % i].pack(side="left")
+    tk.Label(strich_frame, text="_").pack(side="left")
+
+letter_entry = tk.Entry(root, width=10)
+letter_entry.pack(side="bottom", pady=25)
+
+def show():
+    if "e" in letters:
+        for i in range(len(letters)):
+            if letters[i] == "e":
+                globals()["letter%s" %i].configure(text="e")
+
+btn = tk.Button(root, text="Test", command=show)
+btn.pack()
 
 #Run GUI
 root.mainloop()
